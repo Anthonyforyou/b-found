7d83acd9b35ab27764a16bdee262d406a98a0140
Anthony <97813512+Anthonyforyou@users.noreply.github.com>
2024-08-27 20:24:17 +0200

    Create index.js

---
+++ pages/users/index.js
@@ -0,0 +1,11 @@
+// pages/index.js
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welkom op de Homepagina</h1>
+      <p>Dit is de hoofdpagina van je website.</p>
+    </div>
+  );
+};
+
+export default HomePage;
