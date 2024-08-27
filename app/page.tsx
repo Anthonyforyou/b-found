3894d99c99f1ddddde719ce34038f2c646295273
Anthony <97813512+Anthonyforyou@users.noreply.github.com>
2024-08-27 20:36:08 +0200

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+// app/page.tsx
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Welkom op de Hoofdpagina</h1>
+      <p>Dit is de hoofdpagina van je site.</p>
+    </div>
+  );
+}
