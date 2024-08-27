e37a26d3bef1f1ce00949b879fa0ac6cad2bc298
Anthony <97813512+Anthonyforyou@users.noreply.github.com>
2024-08-28 01:21:47 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
-// app/page.tsx
+import React from 'react';
+import styles from './KlantPagina.module.css'; // hergebruiken van dezelfde stijlen
+
 export default function HomePage() {
   return (
-    <div>
-      <h1>Welkom op de Hoofdpagina</h1>
-      <p>Dit is de hoofdpagina van je site.</p>
+    <div className={styles.container}>
+      <h1 className={styles.header}>Informatie</h1>
+      <div className={styles.infoBlock}>
+        <p>Voordat er informatie wordt getoond, moet je een QR-code scannen van D-brand.</p>
+      </div>
     </div>
   );
 }
+
