4191190c48569b970ddcd332a5225a627cb8d9a8
Anthony <97813512+Anthonyforyou@users.noreply.github.com>
2024-08-28 01:39:20 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './KlantPagina.module.css'; // hergebruiken van dezelfde stijlen
+import './globals.css'
 
 export default function HomePage() {
   return (
